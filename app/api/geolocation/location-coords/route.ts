974958e2b0d68b1a0e1ee2015ef744ee63929432
jpham658 <jpham658@gmail.com>
2024-05-23 17:27:34 +0100

    Add skeleton for geolocation API calls

---
+++ app/api/geolocation/location-coords/route.ts
@@ -0,0 +1,5 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function GET(req: NextRequest) {
+    // Get location (lat, lon) coordinates from given location name
+}
