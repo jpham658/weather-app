c641447e11b6c494b8e7df04487fef0ff682f12c
jpham658 <jpham658@gmail.com>
2024-05-23 18:28:13 +0100

    Implement geolocation API calls

--- app/api/geolocation/location-coords/route.ts
+++ app/api/geolocation/location-coords/route.ts
@@ -1,5 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function GET(req: NextRequest) {
-    // Get location (lat, lon) coordinates from given location name
+
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams;
+    const city = searchParams.get("city");
+    const country = searchParams.get("country");
+
+    if(!city) {
+        return new NextResponse("Invalid location...", {
+            status: 400,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+        })
+    }
+    
+    try {
+        const key = process.env.OPEN_WEATHER_API_KEY;
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&appid=${key}`
+        const res = await fetch(url);
+        if (!res.ok) {
+            const errorData = await res.json();
+            return new NextResponse(JSON.stringify({ error: errorData }), {
+              status: res.status,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+          }
+          const data = await res.json();
+          return new NextResponse(JSON.stringify(data), {
+            status: 200,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+    } catch(error) {
+        console.error('Error fetching data:', error);
+    }
 }
--- app/api/geolocation/location-name/route.ts
+++ app/api/geolocation/location-name/route.ts
@@ -1,5 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function GET(req: NextRequest) {
-    // Get location name from given (lat, lon) coordinates
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams;
+    const lat = searchParams.get("lat");
+    const lon = searchParams.get("lon");
+    
+    if(!lat || !lon) {
+        return new NextResponse("Invalid coordinates...", {
+            status: 400,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+        })
+    }
+    
+    try {
+        const key = process.env.OPEN_WEATHER_API_KEY;
+        const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${key}`
+        const res = await fetch(url);
+        if (!res.ok) {
+            const errorData = await res.json();
+            return new NextResponse(JSON.stringify({ error: errorData }), {
+              status: res.status,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+          }
+          const data = await res.json();
+          return new NextResponse(JSON.stringify(data), {
+            status: 200,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+    } catch(error) {
+        console.error('Error fetching data:', error);
+    }
 }
